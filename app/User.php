90ac0e0e7546ab48a3d91021710c89a69c2f0c9a
sugiarto.dlingo@gmail.com <sugiarto.dlingo@gmail.com>
2018-10-26 23:37:22 +0700

    setup facebook login

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'provider', 'provider_id',
     ];
 
     /**
@@ -27,4 +27,16 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    public function addNew($input)
+    {
+        $check = static::where('provider_id',$input['provider_id'])->first();
+
+        if(is_null($check)){
+            return static::create($input);
+        }
+
+        return $check;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('facebook', function () {
+    return view('facebook');
+});
+Route::get('auth/facebook', 'Auth\FacebookController@redirectToFacebook');
+Route::get('auth/facebook/callback', 'Auth\FacebookController@handleFacebookCallback');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
